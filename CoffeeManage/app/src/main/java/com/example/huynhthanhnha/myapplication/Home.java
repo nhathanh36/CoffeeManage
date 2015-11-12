41e5302f7e557bc84f9bd2b0d161cb1816f113d0
ThanhNha <nhathanh36@gmail.com>
2015-11-12 22:33:29 +0700

    AddLogin

---
+++ CoffeeManage/app/src/main/java/com/example/huynhthanhnha/myapplication/Home.java
@@ -0,0 +1,15 @@
+package com.example.huynhthanhnha.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Huynh Thanh Nha on 12-Nov-15.
+ */
+public class Home extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
