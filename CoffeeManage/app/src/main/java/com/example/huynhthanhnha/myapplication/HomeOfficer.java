b2a676782c62a38d201f5f3280db2a6e59afacbb
ThanhNha <nhathanh36@gmail.com>
2015-11-16 19:36:14 +0700

    add folder "anim", add animation in HomeOfficer.

--- CoffeeManage/app/src/main/java/com/example/huynhthanhnha/myapplication/HomeOfficer.java
+++ CoffeeManage/app/src/main/java/com/example/huynhthanhnha/myapplication/HomeOfficer.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 /**
@@ -16,10 +18,14 @@ public class HomeOfficer extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_officer);
 
+        //final Animation animationRotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
         linearLayout = (LinearLayout) findViewById(R.id.linearListTable);
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                linearLayout.startAnimation(anim);
                 startActivity(new Intent(HomeOfficer.this, ListTable.class));
             }
         });
